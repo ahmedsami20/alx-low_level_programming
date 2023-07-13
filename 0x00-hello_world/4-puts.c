4223fa0fc6cda88c88d48b804180d1f50526f4c0
ahmedsami209996 <ahmedsami20996@gmail.com>
2023-07-13 11:38:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("\"Progranning is like building a multilingual puzz");
-    return 0;
+#define PROGRAMMING "Programming"
+
+int main() {
+  printf("Programming is like building a multilingual puzzle\n");
+  return 0;
 }
+
